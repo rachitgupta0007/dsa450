a7bd988ee0ecb3fd1e6163ef398cb41693b05f23
rachitgupta0007 <rachitgupta0007@gmail.com>
2020-12-22 23:34:10 +0530

    update

--- 1-Reverse_the_array.cpp
+++ 1-Reverse_the_array.cpp
@@ -1,22 +1,27 @@
-#include<bits/stdc++.h>
+//reverse array
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
 	int n;
-	cin>>n;
-	int *arr=new int[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
+	cin >> n;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
 	}
-	int i=0,j=n-1;
-	while(i<j){
-		int swap=arr[i];
-		arr[i]=arr[j];
-		arr[j]=swap;
+	int i = 0, j = n - 1;
+	while (i < j)
+	{
+		int swap = arr[i];
+		arr[i] = arr[j];
+		arr[j] = swap;
 		i++;
 		j--;
 	}
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
 	}
 	return 0;
 }
--- 2-max_min_ele_arr.cpp
+++ 2-max_min_ele_arr.cpp
@@ -1,21 +1,28 @@
-#include<bits/stdc++.h>
+//max_min
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
 	int n;
-	cin>>n;
-	int *arr=new int[n];
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
+	cin >> n;
+	int *arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
 	}
-	int max=INT_MIN,min=INT_MAX;
-	for(int i=0;i<n;i++){
-		if(max<arr[i]){
-			max=arr[i];
+	int max = INT_MIN, min = INT_MAX;
+	for (int i = 0; i < n; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
 		}
-		if(min>arr[i]){
-			min=arr[i];
+		if (min > arr[i])
+		{
+			min = arr[i];
 		}
 	}
-	cout<<"Max="<<max<<" "<<"Min="<<min;
+	cout << "Max=" << max << " "
+		 << "Min=" << min;
 	return 0;
 }
